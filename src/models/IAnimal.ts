33173d9378f1743a9382e56ac7479eb8b3c6bfa3
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-27 11:44:17 +0200

    ✨ adding interface Animal

---
+++ src/models/IAnimal.ts
@@ -0,0 +1,12 @@
+export interface IAnimal {
+    id: string;
+    name: string;
+    latinName: string;
+    yearOfBirth: string;
+    shortDescription: string;
+    longDescription: string;
+    imageUrl: string;
+    medicine: string;
+    isFed: boolean;
+    lastFed: string;
+}
