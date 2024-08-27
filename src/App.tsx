77d9931b408b554f1f622c5f8c747c42aa2a5674
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-27 11:29:25 +0200

    ✨ add routerprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { router } from './Router'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
+    <>  
+      <RouterProvider router={router}></RouterProvider>
 
     </>
   )
