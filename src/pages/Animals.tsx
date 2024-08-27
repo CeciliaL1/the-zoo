ef5c8b351d0b23d5b24935ced660c0a3b5b43a08
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-27 13:58:30 +0200

    ✨ render animals

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
+import { IAnimal } from "../models/IAnimal"
+import './styles/animals.css'
+import { useState } from "react"
+
+
 export const Animals = () => {
+
+    const [animals, setAnimals] = useState<IAnimal[]>(JSON.parse(localStorage.getItem('animals') || '[]'))
+
+     
 
     return (
         <>
-            Animals
+            <div className="animals">
+                {animals.map((animal) => (
+                    <Link to={`/animals/${animal.id}`}>
+                        <div className="single-animal" key={animal.id}>
+                            <h3>{animal.name}</h3>
+                            <p>{animal.latinName}</p>
+                            <img src={animal.imageUrl} alt={animal.name} /> 
+                
+                        </div>
+                    </Link>
+                ) )}
+            </div>
         </>
     )
 }
