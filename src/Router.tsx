f8a7c6a166acdb547186e27670f6090bdbad6b82
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-27 13:56:13 +0200

    ✨ adding routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Animals } from "./pages/Animals";
+import { animalLoader, animalsLoader } from "./loaders/AnimalLoader";
+import { Animal } from "./pages/Animal";
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +15,13 @@ export const router = createBrowserRouter([
         },
         {
             path: '/animals',
-            element: <Animals></Animals>
+            element: <Animals></Animals>,
+            loader: animalsLoader
+        },
+        {
+            path: '/animals/:id',
+            element: <Animal></Animal>,
+            loader: animalLoader
         }]
     }
 ])
