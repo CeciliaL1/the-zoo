1534f0e68f2829354d6d8f6f8bf898904263b58d
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-28 12:31:17 +0200

    addinh homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,31 @@
-export const Home = () => {
+import { useLoaderData } from "react-router-dom";
+import { IAnimal } from "../models/IAnimal";
+import './styles/home.css'
 
+export const Home = () => {
+       
+    const animals = useLoaderData() as IAnimal[];
+    
     return (
         <>
-            Home
+           <h2> Välkommen, dina djur i Zoo'et kanske behöver dig!</h2>
+           <p>Här får du en överblick över vilka djur som är hungriga</p>
+           <div className="presentation">
+                {animals.map((animal) => {
+
+                    if (!animal.isFed) {
+                        return (
+                            <>
+                            <div className="short-presentation" key={animal.id}>
+                                <p>{animal.name} är hungrig!</p>
+                                <img src={animal.imageUrl} alt={animal.name} onError={({currentTarget}) => {currentTarget.src = '../public/placeholder-image.jpg'}}/>
+                            </div>
+                                
+                            </>
+                        )
+                    }
+                })}
+           </div>
         </>
     )
 }
