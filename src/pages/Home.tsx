70fd3deba77ad569c1c24bcdab02932432d71644
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-08-29 10:40:31 +0200

    ✨ add render on home is animal is hungry

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,28 +5,27 @@ import './styles/home.css'
 export const Home = () => {
        
     const animals = useLoaderData() as IAnimal[];
+    const hungryAnimals = animals.filter((animal) => animal.isFed != true);
     
     return (
         <>
            <h2> Välkommen, dina djur i Zoo'et kanske behöver dig!</h2>
            <p>Här får du en överblick över vilka djur som är hungriga</p>
-           <div className="presentation">
-                {animals.map((animal) => {
-
-                    if (!animal.isFed) {
-                        return (
-                            <>
-                           
-                            <div className="short-presentation"  key={animal.id} >
-                                <p>{animal.name} är hungrig!</p>
-                                <img src={animal.imageUrl} alt={animal.name} onError={({currentTarget}) => {currentTarget.src = '../public/placeholder-image.jpg'}}/>
-                            </div>                
-                            </>
-                        )
-                    }
-                })}
-             </div>
            
+           <div className="animals">
+           {hungryAnimals.map((animal)  => (
+                <div key={animal.id} className="single-animal" >
+                    <h4>{animal.name} är hungrig!</h4>
+                    <img src={animal.imageUrl} alt={animal.name} onError={({currentTarget}) => {currentTarget.src = '../public/placeholder-image.jpg'}}/>
+                        
+                </div>
+           ))}
+            </div>
+           
+
         </>
     )
 }
+
+
+       
