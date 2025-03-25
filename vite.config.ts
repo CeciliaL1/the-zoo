4d3e58fbfef07f7267d908605739e8dfab088602
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-03-25 13:49:10 +0100

    :sparkles: update base vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/the-zoo-CeciliaL1/",
-})
+  base: "/the-zoo/",
+});
